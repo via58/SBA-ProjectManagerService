8008b6651538998c2208d43fcdbb8679351b9247
Admin <Admin@DOTNET>
2018-12-17 10:46:10 -0800

    Final Commit

---
+++ ProjectManager.PerformanceTest/PerformanceTesting.cs
@@ -0,0 +1,41 @@
+﻿using NBench;
+using ProjectManager.ServiceLayer.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.PerformanceTest
+{
+    public class PerformanceTesting
+    {
+
+        [PerfBenchmark(NumberOfIterations = 1, RunMode = RunMode.Throughput,
+        TestMode = TestMode.Test, SkipWarmups = true)]
+        [ElapsedTimeAssertion(MaxTimeMilliseconds = 5000)]
+        public void Benchmark_Performance_ElaspedTime()
+        {
+            var controller = new TaskController();
+            controller.Request = new HttpRequestMessage()
+            {
+                RequestUri = new Uri("http://localhost")
+            };
+            controller.GetAllTasks();
+        }
+
+        [PerfBenchmark(Description = "MemoryTEST",
+NumberOfIterations = 5, RunMode = RunMode.Throughput, RunTimeMilliseconds = 2500, TestMode = TestMode.Test)]
+        [MemoryAssertion(MemoryMetric.TotalBytesAllocated, MustBe.LessThanOrEqualTo, ByteConstants.SixtyFourKb)]
+        public void Benchmark_Performance_Memory()
+        {
+            var controller = new TaskController();
+            controller.Request = new HttpRequestMessage()
+            {
+                RequestUri = new Uri("http://localhost")
+            };
+            controller.GetAllTasks();
+        }
+    }
+}
